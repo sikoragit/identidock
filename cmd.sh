2d08b5e3135d9eb4d05f21f59cb36f52c6bd0be3
sikoragit <jvsikora@roadrunner.com>
2016-12-28 11:05:30 -0800

    Find default working directory

--- cmd.sh
+++ cmd.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
+pwd
+
 if [[ $ENV == 'DEV' ]]; then 
+pwd
    echo "Running Development Server test"
    exec python "identidock.py"
 elif [[ $ENV == 'UNIT' ]]; then
+pwd
     echo "Running Unit Tests"
-    exec python "app/tests.py"
+    exec python "tests.py"
 else
+pwd
    echo "Running Production Sever"
    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
                      --callable app --stats 0.0.0.0:9191
