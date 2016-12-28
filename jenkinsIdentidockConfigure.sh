04aaec3d8028f0377be54f7e9bd58aa3f798a5e2
sikoragit <jvsikora@roadrunner.com>
2016-12-28 10:50:36 -0800

    commands for Jenkins build and test

---
+++ jenkinsIdentidockConfigure.sh
@@ -0,0 +1,35 @@
+# File used for Identidock Unit tests in Jenkins
+
+# Default compose args
+COMPOSE_ARGS=" -f jenkins.yml -p jenkins "
+
+# Make sure old containers are gone
+sudo docker-compose $COMPOSE_ARGS stop
+sudo docker-compose $COMPOSE_ARGS rm --force -v
+
+# Build the system
+sudo docker-compose $COMPOSE_ARGS build --no-cache
+sudo docker-compose $COMPOSE_ARGS up -d
+
+# Run unit tests
+sudo docker-compose $COMPOSE_ARGS run --no-deps --rm \
+                    -e ENV=UNIT identidock
+ERR=$?
+
+# Run system test if unit tests passed
+if [[ $ERR == 0 ]]; then
+	IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} \
+         jenkins_identidock_1)
+    CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) \
+       || true
+    if [[ $CODE != 200 ]; then
+    	echo "Site returned " $Code
+       	ERR=1
+    fi
+fi
+
+# Pull down the system
+sudo docker-compose $COMPOSE_ARGS stop
+sudo docker-compose $COMPOSE_ARGS rm --force -v
+
+return $ERR
